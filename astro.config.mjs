af1a4ca5ae862368334d7892c1a6a3734a0c7f42
EViL92 <villescas.librado@gmail.com>
2026-03-20 23:53:02 -0300

    Web estatica pura

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  output: 'static',
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
